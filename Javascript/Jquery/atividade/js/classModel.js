0e6833359d1654abd185fb12327e740327b4a4b0
AmauryIII <t3rceir02018@gmail.com>
2024-06-13 15:10:01 -0300

    update atividades

---
+++ Javascript/Jquery/atividade/js/classModel.js
@@ -0,0 +1,10 @@
+class produtoTurismo {
+    constructor (nome, descLocal, veiculo, duracao, valor, imagem){
+        this.nome = nome
+        this.descLocal = descLocal 
+        this.veiculo = veiculo
+        this.duracao = duracao
+        this.valor = valor
+        this.imagem = imagem
+    }
+}
